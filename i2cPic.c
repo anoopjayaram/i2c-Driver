7d19d2beb33f12767c28305ccadfa3709f17dce2
Anoop Jayaram <66719752+anoopjayaram@users.noreply.github.com>
2023-11-04 11:40:45 +0530

    Update i2cPic.c

--- i2cPic.c
+++ i2cPic.c
@@ -1,28 +1,50 @@
 #include<pic18.h>
-void i2c_init()
+void I2C_INIT();
+void _I2C_STOP();
+void I2C_SEND_DATA(char a);
+char I2C_READ_DATA(char a);
+void _I2C_START();
+void main()
+{
+TRISB=TRISD=0x00;
+TRISC=0x18;
+for(long i=0;i<100000;i++);//delay
+I2C_INIT();
+_I2C_START();//start slave ad+R/w  
+for(long i=0;i<100000;i++);//delay
+I2C_SEND_DATA(0xd0);//1101 000 0
+I2C_SEND_DATA('S');
+I2C_SEND_DATA('M');
+_I2C_STOP(); 
+while(1);
+
+}
+
+void I2C_INIT()
 {
 SSPSTAT=0x00;//1000
 SSPCON1=0x28;//
 SSPCON2=0x00;
 SSPADD=49;
 } 
-void i2c_start()
+void _I2C_START()
 {
 SEN=1;
-while(SEN==1);// start bit
+while(SEN==1);//
 }
-void i2c_stop()
+
+void _I2C_STOP()
 {
 PEN=1;
-while(PEN==1);// stop bit
+while(PEN==1);// 
 }
-void i2c_data(char a)
+void I2C_SEND_DATA(char a)
 {
 SSPIF=0;
 SSPBUF=a;
 while(SSPIF==0);
 }
-char i2c_read(char a)
+char I2C_READ_DATA(char a)
 {
 RCEN=1;//receive
 while(RCEN);//
@@ -31,27 +53,3 @@ ACKEN=1;
 while(ACKEN);
 return SSPBUF;
 }
-void main()
-{
-
-TRISB=TRISD=0x00;
-TRISC=0x18;
-for(long i=0;i<100000;i++);
-i2c_init();
-i2c_start();//start slave ad+R/w  
-for(long i=0;i<100000;i++);
-i2c_data(0xd0);//1101 000 0
-i2c_data('S');
- 
-i2c_data('M');
-
-i2c_stop(); 
-
-
-while(1)
-{
-
-
-}
-}
-
